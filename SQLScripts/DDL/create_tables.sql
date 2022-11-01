b3c0a51256d902815e990315822d814d389a2369
Raunak Poddar <2021mt12277@wilp.bits-pilani.ac.in>
2022-11-01 18:30:35 +0530

    CRUD operations for Student

---
+++ SQLScripts/DDL/create_tables.sql
@@ -0,0 +1,26 @@
+-- sqlite3 vaccinedb.sqlite3
+
+CREATE TABLE IF NOT EXISTS "Student" (
+	"StudentID"	INTEGER NOT NULL UNIQUE,
+	"Name"	TEXT NOT NULL,
+	"Email"	TEXT NOT NULL UNIQUE,
+    "DoB" TEXT NOT NULL,
+    "AadharNumber" TEXT,
+	"MobileNumber" INTEGER NOT NULL UNIQUE,
+	"Status" TEXT NOT NULL DEFAULT 'ACTIVE' CHECK(Status IN ('ACTIVE','INACTIVE')),
+    "VaccinationStatus" TEXT NOT NULL DEFAULT 'DONE' CHECK(Status IN ('DONE','PENDING', 'MEDICAL_ISSUES_CANNOT_VACCINATE', 'UNKNOWN', 'SCHEDULED')),
+    "VaccineType" TEXT,
+    "VaccinationWorkerId" TEXT,
+    "VaccinationDate" TEXT,
+	PRIMARY KEY("StudentID")
+);
+
+-- INDEXES
+
+CREATE INDEX "StudentIndexes" ON "Student" (
+	"Name"	DESC,
+	"Status" DESC,
+    "VaccinationStatus" ASC,
+    "VaccineType" ASC,
+    "VaccinationDate" DESC
+);
--- app.js
+++ app.js
@@ -1,8 +1,107 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const fs = require('fs');
+const sqlite3 = require("sqlite3").verbose();
 
-app.get("/test", (req, res) => {
-    res.status(200).json({success : true});
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
+
+const db_name = path.join(__dirname, "data", "vaccinedb.sqlite3");
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'vaccinedb.sqlite3'");
 });
 
-app.listen(3030, () => console.log("server is running on port 3030"));
+app.listen(3090, () => {
+  console.log("Server started (http://localhost:3090/) !");
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/students", (req, res) => {
+  const sql = "SELECT * FROM Student"
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("students", { model: rows });
+  });
+});
+
+// GET /students/create
+app.get("/students/create", (req, res) => {
+  res.render("student-create", { model: {} });
+});
+
+// POST /students/create
+app.post("/students/create", (req, res) => {
+  console.log(req)
+  const sql = "INSERT INTO Student (Name, Email, DoB, AadharNumber, MobileNumber, Status, VaccinationStatus, VaccineType, VaccinationWorkerId, VaccinationDate) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  const student = [req.body.Name, req.body.Email, req.body.DoB, req.body.AadharNumber, req.body.MobileNumber, req.body.Status, req.body.VaccinationStatus, req.body.VaccineType, req.body.VaccinationWorkerId, req.body.VaccinationDate];
+  db.run(sql, student, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/students");
+  });
+});
+
+// GET /students/edit/5
+app.get("/students/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Student WHERE StudentID = ?";
+  db.get(sql, id, (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("student-edit", { model: row });
+  });
+});
+
+// POST /students/edit/5
+app.post("/students/edit/:id", (req, res) => {
+  const id = req.params.id;
+  const student = [req.body.Name, req.body.Email, req.body.DoB, req.body.AadharNumber, req.body.MobileNumber, req.body.Status, req.body.VaccinationStatus, req.body.VaccineType, VaccinationWorkerId, VaccinationDate, id];
+  const sql = "UPDATE Student SET Name = ?, Email = ?, DoB = ?, AadharNumber = ?, MobileNumber = ?, Status = ?, VaccinationStatus = ?, VaccineType = ?, VaccinationWorkerId = ?, VaccinationDate = ?  WHERE (StudentID = ?)";
+  db.run(sql, student, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/students");
+  });
+});
+
+// GET /students/delete/5
+app.get("/students/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "SELECT * FROM Student WHERE StudentID = ?";
+  db.get(sql, id, (err, row) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.render("delete", { model: row });
+  });
+});
+
+// POST students/delete/5
+app.post("/students/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const sql = "DELETE FROM Student WHERE StudentID = ?";
+  db.run(sql, id, err => {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.redirect("/students");
+  });
+});
